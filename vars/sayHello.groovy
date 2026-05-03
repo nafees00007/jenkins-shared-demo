0610de7a4453f14d8fd76d8b8de142e0880a1bc0
Nafees Anwer <127747455+nafees00007@users.noreply.github.com>
2026-05-03 18:00:27 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,7 +4,7 @@ def call(Map config) {
         stages {
             stage('Print Info') {
                 steps {
-                    echo "Running in ${config.env} environment"
+                    echo "Runninggggggggggggggggggggg in ${config.env} environment"
                 }
             }
         }
